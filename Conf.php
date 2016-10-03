217a3f2b00aac8e099e9887b00fe7335a6102ee4
Zakin34 <crash34@hotmail.fr>
2016-10-03 14:24:00 +0200

    Create Conf.php

---
+++ Conf.php
@@ -0,0 +1,36 @@
+<?php
+
+class Conf {
+
+    private static $database = array(
+        'hostname' => 'localhost', 
+        'database' => 'nomtable', 
+        'login' => 'nomtable',
+        'password' => 'codetable'
+    );
+    
+    private static $debug = true;
+
+    static public function getLogin() {
+        return self::$database['login'];
+    }
+
+    static public function getHostname() {
+        return self::$database['hostname'];
+    }
+
+    static public function getDatabase() {
+        return self::$database['database'];
+    }
+
+    static public function getPassword() {
+        return self::$database['password'];
+    }
+    
+    static public function getDebug() {
+        return self::$debug;
+    }
+
+}
+
+?>
